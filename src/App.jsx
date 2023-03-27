55c83fe239e46d7993375cbadbef843ee8106ac1
ovi-khan <manikkhan2240@gmail.com>
2023-03-27 22:20:52 +0600

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import Shop from './Components/Shop/Shop'
 
 function App() {
  
   return (
     <div className="App">
      <Header></Header>
+     <Shop></Shop>
     </div>
   )
 }
